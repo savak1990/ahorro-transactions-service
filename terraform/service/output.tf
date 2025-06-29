c53d70e6511693aed5ca7e19d6012d273b328835
Viacheslav Klovan <slava.klovan@gmail.com>
2025-06-29 14:02:06 +0200

    Add missing module outputs for API Gateway ID and Lambda function name

--- terraform/service/output.tf
+++ terraform/service/output.tf
@@ -2,3 +2,18 @@ output "service_url" {
   description = "The URL of the ahorro transactions service."
   value       = module.apigateway.api_gateway_url
 }
+
+output "api_url" {
+  description = "The URL of the transactions service API."
+  value       = module.apigateway.api_gateway_url
+}
+
+output "api_gateway_id" {
+  description = "The ID of the API Gateway."
+  value       = module.apigateway.http_api_id
+}
+
+output "lambda_function_name" {
+  description = "The name of the Lambda function."
+  value       = aws_lambda_function.app.function_name
+}
